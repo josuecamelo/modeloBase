8c311b187a62dcf1ddaef0f1b444d1f180c50649
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-03-31 08:32:33 -0300

    local storage

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # modeloBase
 Projeto Base Ionic 3 e Angular 5 - Integrado com Laravel 5.6
+
+#faltando criar
+* criar serviço de mensagem para centralizar resposta no geral
+* criar auth service para fazer autenticação
+* criar interceptador de requisições para tratar http 401
--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,14 +1,13 @@
 /**
  * Created by JCamelo on 30/03/2018.
  */
+
 import { ApiProvider } from './api/api';
-//import { Items } from '../mocks/providers/items';
-//import { Settings } from './settings/settings';
+import { SettingsProvider } from './settings/settings';
 import { UserProvider } from './user/user';
 
 export {
     ApiProvider,
-    //Items,
-    //Settings,
+    SettingsProvider,
     UserProvider
 };
