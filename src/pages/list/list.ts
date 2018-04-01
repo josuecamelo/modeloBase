ac3cb22ac262ab320eb21db39ecaa46e11c0ad8f
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-01 00:00:27 -0300

    GRAVANDO DADOS COM SQLITE

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,39 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {DbAppProvider} from "../../providers/db-app/db-app";
+import {SQLiteObject} from "@ionic-native/sqlite";
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  /*selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  contatos = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbApp: DbAppProvider) {
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
+  ionViewDidLoad() {
+    this.dbApp.getSqlLiteInstanace().then((db: SQLiteObject) => {
+      db.executeSql('SELECT * FROM contatos',[]).then((rs)=> {
+        for(let i = 0; i <= rs.rows.length; i++){
+          let contato = rs.rows.item(i);
+          this.contatos.push(contato);
+        }
+      }).catch((error) => console.log(error));
     });
-  }*/
-
-
+  }
 }
