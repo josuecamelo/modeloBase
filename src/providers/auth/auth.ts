5a9ee7c91fbd15662bfe9c215e492da170198675
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-02 17:55:42 -0300

    Manipulação de Dados SqlLite - Funcional

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {ApiProvider} from "../api/api";
+import {DbAppProvider} from "../db-app/db-app";
+import {SQLiteObject} from "@ionic-native/sqlite";
 
 @Injectable()
 export class AuthProvider {
@@ -9,7 +11,7 @@ export class AuthProvider {
 
   redirectAfterLogin = 'ListPage';
 
-  constructor(private api: ApiProvider )
+  constructor(private api: ApiProvider, private dbApp: DbAppProvider )
   {
     this.check = this._token ? true : false;
   }
@@ -26,10 +28,42 @@ export class AuthProvider {
         alert( res.itemValue );
       });*/
 
-      //gravar token no banco
-
+      //gravar token no banco //gravando token para requisiçoes futura
+      this.gravarToken(this._token);
+      this.obterToken();
+      console.log('token obtido->>' + this._token);
     });
 
     return seq;
+  }
+
+  //GESTÃO DE TOKEN
+  gravarToken(token: any){
+    this.dbApp.getSqlLiteInstanace().then((db: SQLiteObject) => {
+      db.executeSql('INSERT INTO autenticacao(token) values(?)',[token]).then(()=> {
+        //
+      }).catch((error) => console.log(error));
+    }).catch(e => console.log(e));
+  }
+
+  obterToken(){
+    this.dbApp.getSqlLiteInstanace().then((db: SQLiteObject) => {
+      db.executeSql('SELECT * FROM autenticacao WHERE id = 1',[]).then((rs)=> {
+        for(let i = 0; i <= rs.rows.length; i++){
+          let dadosAuth = rs.rows.item(i);
+          this._token = dadosAuth.token;
+        }
+      }).catch((error) => console.log(error));
+    });
+
+    return this._token;
+  }
+
+  atualizarToken(token:any){
+    this.dbApp.getSqlLiteInstanace().then((db: SQLiteObject) => {
+      db.executeSql('UPDATE autenticacao SET token = ? WHERE id = 1',[token]).then(()=> {
+        this._token = token;
+      }).catch((error) => console.log(error));
+    }).catch(e => console.log(e));
   }
 }
