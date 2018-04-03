545f616276019215a29d1767a754c01b9110a436
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-03 10:31:33 -0300

    Manipulação de Dados SqlLite - Funcional

--- src/providers/db-app/db-app.ts
+++ src/providers/db-app/db-app.ts
@@ -4,10 +4,10 @@ import {SQLite, SQLiteObject} from "@ionic-native/sqlite";
 
 const DATABASE_SCHEMA = [
     `DROP TABLE IF EXISTS operadoras`,
-    `CREATE TABLE IF NOT EXISTS operadoras(
+    /*`CREATE TABLE IF NOT EXISTS operadoras(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         nome VARCHAR(255)                        
-    )`,
+    )`,*/
     `DROP TABLE IF EXISTS contatos`,
     `CREATE TABLE IF NOT EXISTS contatos(
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -24,18 +24,18 @@ const DATABASE_SCHEMA = [
 ];
 
 const OPERADORAS_DATA = [
-    ['INSERT INTO operadoras(nome) values(?)',['CLARO']],
+    /*['INSERT INTO operadoras(nome) values(?)',['CLARO']],
     ['INSERT INTO operadoras(nome) values(?)',['OI']],
     ['INSERT INTO operadoras(nome) values(?)',['VIVO']],
-    ['INSERT INTO operadoras(nome) values(?)',['TIM']],
-    ['INSERT INTO contatos(nome, email, telefone, operadora_id) values(?,?,?,?)',['Josué Camelo',
+    ['INSERT INTO operadoras(nome) values(?)',['TIM']],*/
+    /*['INSERT INTO contatos(nome, email, telefone, operadora_id) values(?,?,?,?)',['Josué Camelo',
         'Josué Camelo', '(62) 99252-7138', 1]],
     ['INSERT INTO contatos(nome, email, telefone, operadora_id) values(?,?,?,?)',['Doraci Camelo',
         'Doraci Camelo', '(62) 99252-7139', 1]],
     ['INSERT INTO contatos(nome, email, telefone, operadora_id) values(?,?,?,?)',['Daniel Camelo',
         'Daniel Camelo', '(62) 99252-7140', 1]],
     ['INSERT INTO contatos(nome, email, telefone, operadora_id) values(?,?,?,?)',['Priscila Camelo',
-        'Priscila Camelo', '(62) 99252-7141', 1]],
+        'Priscila Camelo', '(62) 99252-7141', 1]],*/
 ];
 
 @Injectable()
